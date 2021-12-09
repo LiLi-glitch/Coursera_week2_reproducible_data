1d96f78383dc161349e12fc25e226b9c3f911997
LiLi <60715815+LiLi-glitch@users.noreply.github.com>
2021-12-09 22:15:44 +0100

    start

---
+++ Week 2 script .R
@@ -0,0 +1,14 @@
+library(readxl)
+library(data.table)
+library(ggplot2)
+
+#read in csv 
+activity <-fread("./activity.csv")
+
+head(activity)
+
+#calculate mean steps per day by aggregating
+stepsday <- aggregate(steps ~ date, activity, FUN = mean)
+#make a histogram
+ggplot(stepsday, aes(x=steps)) +
+geom_histogram()
